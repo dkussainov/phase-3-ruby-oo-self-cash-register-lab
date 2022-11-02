a1714d832ced7ff6c93a267168e08b284500a257
Dulat <dulat.kussainov@gmail.com>
2022-11-02 16:23:15 -0400

    commit tree 07ba7a22f00a3eed1f2b46997ac4ba97c6faa1a0

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,19 @@
+require 'pry'
 
+class CashRegister
+    attr_accessor :discount, :total, :items, :last_transaction
+    def initialize(discount=0)
+        @discount = discount
+        @total = 0
+        @items=[]
+
+    end
+    def add_item(title, amount, quantity = 1 )
+        self.total += self.amount * self.quantity
+        
+       
+        end
+        
+    end
+
+end
